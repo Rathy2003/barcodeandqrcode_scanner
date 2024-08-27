ffc1b72f536ac0c30cc7a4fa4a8244fade3a4ade
rathy2303 <thyjks2020@gmail.com>
2024-08-27 11:14:03 +0700

    Update UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp
+    ]
+  );
   runApp(const MyApp());
 }
 
@@ -64,10 +69,21 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _showDiaglog(BuildContext context)async {
-  return showDialog(context: context, builder: (context) {
+  return showDialog(
+      context: context,
+      builder: (context) {
     return AlertDialog(
       title: Text(""),
-      content: Text(qrcoderesult),
+      content: Text(qrcoderesult,style: TextStyle(
+          fontSize: 17),
+      textAlign: TextAlign.center,
+      ),
+      insetPadding: EdgeInsets.zero,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      contentPadding: EdgeInsets.all(14),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8)
+      ),
       actions: [
         TextButton(
             onPressed: (){
@@ -101,51 +117,41 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Barcode and QRCode Scanner"),
-      ),
+      backgroundColor: Colors.black38,
       body: Container(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(qrcoderesult),
-              SizedBox(height: 10,),
-              Container(
-                width: 190,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(8)
-                ),
-                child: ElevatedButton(
-                    onPressed: (){
-                      scanQRCode();
-                    },
-                    child: Icon(Icons.qr_code,color: Colors.white,size: 33,),
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    backgroundColor: Colors.transparent,
+              InkWell(
+                onTap: (){
+                  scanQRCode();
+                },
+                child: Container(
+                  width: 190,
+                  height: 60,
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF121212),
+                    borderRadius: BorderRadius.circular(8)
                   ),
+                  child: Image.asset("assets/images/qr-code.png"),
                 ),
               ),
-              SizedBox(height: 15,),
-              Container(
-                width: 190,
-                height: 60,
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(8)
-                ),
-                child: ElevatedButton(
-                  onPressed: (){
-                    scanBarCode();
-                  },
-                  child: Icon(Icons.code,color: Colors.white,size: 33,),
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    backgroundColor: Colors.transparent,
+              SizedBox(height: 25,),
+              InkWell(
+                onTap: (){
+                  scanBarCode();
+                },
+                child: Container(
+                  width: 190,
+                  height: 60,
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Color(0xFF121212),
+                      borderRadius: BorderRadius.circular(8)
                   ),
+                  child: Image.asset("assets/images/barcode.png",)
                 ),
               )
             ],
